b743ef92cfe24295898070e588580633285b5b85
Alex Mykyta <amykyta3@users.noreply.github.com>
2023-07-28 23:16:13 -0700

    Change theme

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@
 project = 'Plast Karto'
 copyright = '2020, Alex Mykyta'
 author = 'Alex Mykyta'
+html_title = project
 
 
 # -- General configuration ---------------------------------------------------
@@ -44,7 +45,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_book_theme"
+
+html_theme_options = {
+    "repository_url": "https://github.com/amykyta3/plast-karto",
+    "path_to_docs": "docs",
+    "use_download_button": False,
+    "use_source_button": False,
+    "use_repository_button": True,
+    "use_issues_button": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
